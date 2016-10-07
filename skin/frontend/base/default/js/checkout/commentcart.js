dab165453f16b182396f2ad818bc5e5996547dff
Oliver Sokola <os@kirchbergerknorr.de>
2016-10-07 17:11:15 +0200

    ajax request logic updated to avoid null comments

--- skin/frontend/base/default/js/checkout/commentcart.js
+++ skin/frontend/base/default/js/checkout/commentcart.js
@@ -1,8 +1,34 @@
 (function($) {
+
     var currentUpdate = null;
+    var timerHandle = null;
+    var isTimerRunning = false;
+    var updateInterval = 750;
+
+    /**
+     * Timer object to start ajax request when comment typing has ceased,
+     * with slight delay, timer will reset when typing resumes during delay
+     */
+    var startCommentTimer = new function () {
+        this.run = function () {
+            if(isTimerRunning) {
+                clearTimeout(timerHandle);
+            }
+            else {
+                isTimerRunning = true;
+            }
+
+            timerHandle = setTimeout(function () {
+                isTimerRunning = false;
+                timerHandle = null;
+                updateComment();
+            }, updateInterval);
+        }
+    };
+
 
     var updateComment = function() {
-        var commentElement = $(this);
+        var commentElement = $('#commentcart');
         var comment = commentElement.val();
         currentUpdate = $.ajax({
             type: 'POST',
@@ -20,5 +46,5 @@
             }
         });
     };
-    $(document).on('change', '#commentcart', updateComment);
+    $(document).on('keypress', '#commentcart', startCommentTimer.run);
 } (jQuery));
